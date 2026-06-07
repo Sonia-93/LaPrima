b736d4243702f4cccf369964861f5795e2007744
Wateta <shimirwasonia5@gmail.com>
2026-06-07 22:08:11 +0200

    update server.js

--- server.js
+++ server.js
@@ -6,22 +6,36 @@ const authRoutes = require("./authentication_Mongo/route/auth.routes");
 const menuRoutes = require("./routes/menu");
 const orderRoutes = require("./routes/order");
 const customerRoutes = require("./routes/customer");
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
 connectDB();
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "OK",
+    message: "La Prima API is running 🚀",
+  });
+});
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api", menuRoutes);
 app.use("/api", orderRoutes);
 app.use("/api", customerRoutes);
 
+
 app.use((req, res) => {
   res.status(404).json({ message: "Route not found" });
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
